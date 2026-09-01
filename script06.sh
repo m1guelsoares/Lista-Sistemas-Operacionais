501a0e149b2ef8d7964f348782bf56774d0ecf29
Miguel Correia Soares <miguelcsoares12@gmail.com>
2026-09-01 19:19:48 -0300

    Add script to search for a word in a file

--- script06.sh
+++ script06.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+read -p " Palavra a ser buscada: " pal
+read -p " Nome do arquivo: " arq
+
+if [ -f "$arq" ]; then
+	if grep -q "$pal" "$arq"; then
+		echo "Busca concluída"
+	else
+		echo " Palavra não encontrada " 
+fi 
+else 
+	echo"Arquivo inexistente"
+fi
 
