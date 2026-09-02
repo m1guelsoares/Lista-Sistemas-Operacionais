0d9e17cb1556143a2add334707b7ffb7435f5c18
Miguel Correia Soares <miguelcsoares12@gmail.com>
2026-09-02 00:09:28 -0300

    Update script08.sh

--- script08.sh
+++ script08.sh
@@ -3,7 +3,7 @@ read -p "Arquivo 1:" a1
 read -p "Arquivo 2:" a2
 read -p "Diretório destino" dd
 
-if [ -f $a1 ] && [ -f $a2 ]; then
+if [ -f "$a1" ] && [ -f "$a2" ]; then
 	cat "$a1" "$a2" > "$dd"
 	echo "Arquivos concatenados"
 else
