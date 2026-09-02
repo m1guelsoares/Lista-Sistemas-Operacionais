f95b116f75b72de6e3a31c64f72dbeb2b0920c5d
Miguel Correia Soares <miguelcsoares12@gmail.com>
2026-09-01 23:56:34 -0300

    Add command menu for file search options

--- script07.sh
+++ script07.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+echo "MENU de comandos de busca"
+echo "1-find"
+echo "2-locate"
+echo "3-whereis"
+read -p "Escolha o dígito(1-4): " dig
+
+case $dig in
+	1)
+read -p "Nome do arquivo:" na1
+find . -name "$na1"
+	;;
+	2)
+		read -p "Nome do arquivo:" na2
+		locate "$na2"
+	;;
+	3)
+		read -p "Formato do comando:" c
+		whereis "$c"
+	;;
+esac
