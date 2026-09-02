e62ac42532c9fc1a91134fce82e9714d48884c33
Miguel Correia Soares <miguelcsoares12@gmail.com>
2026-09-02 00:01:26 -0300

    Add script to concatenate two files
    The script concatenates two files if they exist and outputs the result to a specified directory.

--- script08.sh
+++ script08.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+read -p "Arquivo 1:" a1
+read -p "Arquivo 2:" a2
+read -p "Diretório destino" dd
+
+if [ -f $a1 ] && [ -f $a2 ]; then
+	cat "$a1" "$a2" > "$dd"
+	echo "Arquivos concatenados"
+else
+	echo "arquivos inválidos"
+fi
 
